d96089b297e6c75b058918a615c5e496dfc3f48e
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-01-08 13:57:34 +0100

    initial commit

---
+++ validate.js
@@ -0,0 +1,56 @@
+$(function () {
+    /*this area is for the display of an error when users keeps typing spaces*/
+    $.validator.addMethod("noSpace", function (value, element) {
+        return value == '' || value.trim().length != 0
+    }, "spaces are not allowed")
+
+    $("#register").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+
+            fname: {
+                required: true,
+                noSpace: true
+            }
+
+        },
+        message: {
+            email: {
+                required: 'please enter email.',
+                email: 'enter a valid email!'
+            },
+            fname: {
+                required: 'enter name'
+            }
+        }
+
+
+    })
+})
+
+
+
+
+
+
+
+/*$(function () {
+    var $registerForm = $("#register");
+    if ($registerForm.length) {
+        $registerForm.validate({
+            rules: {
+                email: {
+                    required: true,
+                }
+            },
+            message: {
+                email: {
+                    required: 'please enter email!'
+                }
+            }
+        })
+    }
+})*/
